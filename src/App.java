37e283ba645ef63410c5190d63e28c8978135b63
Zsolt <zsolt@example.com>
2021-09-06 09:05:25 +0200

    Init

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/*
+* File: App.java
+* Author: Juhász Zsolt
+* Copyright: 2021, Juhász Zsolt
+* Group: Szoft 2 N
+* Date: 2021-09-06
+* Github: https://github.com/ZsoltJuhasz/
+* Licenc: GNU GPL
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
